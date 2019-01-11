2db80f3571950b4cd220bdeca10445677f628045
mkhainfo <to@mkha.info>
2019-01-11 14:09:27 -0800

    title card available, styling begins

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const TitleCard = props => {
+  return (
+    <div>
+      <h1>mkha.info</h1>
+      <h2><a href="mailto:contact@mkha.info">contact@mkha.info</a></h2>
+    </div>
+  )
 }
 
-export default App;
+const Gallery = props => {return(<div />)}
+
+export default class App extends Component {
+  render() {
+    return (
+      <div id="container">
+        <TitleCard />
+        <Gallery />
+      </div>
+    )
+  }
+}
