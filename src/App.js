114db90a9151ec9fe7ac74b398e3830109712e23
mkhainfo <to@mkha.info>
2019-01-18 20:24:53 -0800

    info card placed, size to be adjusted

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react'
 import './App.css'
 
-/*const TitleCard = props => {
+const InfoCard = props => {
   return (
     <div style={styles.card}>
       <h1>mkha.info</h1>
-      <h2><a href="mailto:contact@mkha.info">contact@mkha.info</a></h2>
+      <h2 style={{backgroundColor: 'blue'}}><a href="mailto:contact@mkha.info">contact@mkha.info</a></h2>
     </div>
   )
-}*/
+}
 
-class Gallery extends Component {
+class Slides extends Component {
   constructor(props){
     super(props)
     this.state = {
@@ -26,7 +26,7 @@ class Gallery extends Component {
 
         let slides = []
         // pushes n divs (slide) into array (slides)
-        for (let i=0 ; i < 10 ; i++) {
+        for (let i=0 ; i < 100 ; i++) {
           slides.push(
             <div key={i}
               style={{
@@ -44,16 +44,15 @@ class Gallery extends Component {
   columns = () => {
     const n = Math.floor(window.innerWidth / 300)
     let total = n % 2 === 0 ? n : n - 1 > 0 ? n - 1 : n,
-      gutters = window.innerWidth / (total * 10),
-      margin = gutters * 2 + 100,
-      width = (window.innerWidth - (gutters * (total - 1)) - margin) / total
+      gutter = window.innerWidth / (total * 10),
+      margin = gutter * 2 + 100,
+      width = (window.innerWidth - (gutter * (total - 1)) - margin) / total
       //width = (window.innerWidth - 100) / total
-
     return {
       gridTemplateColumns: `repeat(${total}, ${width}px)`,
       gridAutoRows: `${width}px`,
-      gridGap: `${gutters}px`,
-      marginTop: margin,
+      gridGap: `${gutter}px`,
+      marginTop: gutter,
       marginBottom: margin,
     }
   }
@@ -61,7 +60,7 @@ class Gallery extends Component {
 
   render() {
     return(
-      <div style={{...styles.gallery, ...this.columns()}}>
+      <div style={{...styles.slides, ...this.columns()}}>
         {this.state.slides}
       </div>
     )
@@ -96,7 +95,10 @@ export default class App extends Component {
 
     return (
       <div style={styles.container}>
-        <Gallery />
+        <InfoCard />
+        <div style={{...styles.container, ...styles.slideContainer}}>
+          <Slides />
+        </div>
       </div>
     )
   }
@@ -108,21 +110,28 @@ export default class App extends Component {
 const styles = {
 
   container: {
-  	width: '90vw',
+    width: '90vw',
   	height: 'auto',
-    display: 'flex',
     margin: 'auto',
+  },
+
+  slideContainer: {
+    display: 'flex',
     justifyContent: 'center',
   },
 
-/*  card: {
-    zIndex: 2,
-  	alignSelf: 'flex-start',
-  	backgroundColor: '#ffffff',
-  	padding: '.25vmin 5vmin 0.25vmin 15vmin',
-  },*/
+  card: {
+    zIndex: 5,
+    //position: '-webkit-sticky',
+    position: 'sticky',
+    top: '20px',
+    margin: 0,
+    padding: '5vw',
+    width: '50%',
+    backgroundColor: 'cyan',
+  },
 
-  gallery: {
+  slides: {
     //backgroundColor: 'cyan',
     display: 'inline-grid',
     gridAutoFlow: 'row',
