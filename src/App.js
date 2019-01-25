0f30515cc8225f4328aaf7604d6cd23d1a31fc13
mkhainfo <to@mkha.info>
2019-01-25 14:10:44 -0800

    modal is active, opens on slide click, closes on scroll away and close button click

--- src/App.js
+++ src/App.js
@@ -24,12 +24,25 @@ class InfoCard extends Component {
     }
   }
 
+  closeModal = e => {
+    this.props.toggleModal(false)
+    console.log('close')
+  }
+
   componentDidMount() {
     let w = document.getElementById('fit').offsetWidth
     this.setState({w})
   }
+
   render() {
     return (
+      this.props.displayModal ?
+      <div style={{...styles.card, ...this.cardStyles()}}>
+        <h1>title <button onClick={this.closeModal}>(close)</button> </h1>
+        <h2><span id="fit"><a href="mailto:contact@mkha.info">contact@mkha.info</a>
+        </span></h2>
+      </div>
+      :
       <div style={{...styles.card, ...this.cardStyles()}}>
         <h1>mkha.info</h1>
         <h2><span id="fit"><a href="mailto:contact@mkha.info">contact@mkha.info</a>
@@ -51,14 +64,15 @@ class Slides extends Component {
         // pushes n divs (slide) into array (slides)
         for (let i=0 ; i < 100 ; i++) {
           slides.push(
-            <div key={i}
+            <button key={i}
               className={'slide'}
-              onClick={this.handleClick}
+              onClick={this.displayModal}
               style={{
               backgroundColor: aColor(),
               gridColumnEnd: 'span 1',
               placeSelf: 'stretch',
-            }}></div>
+              zIndex: 1,
+            }}/>
           )
         }
         return slides
@@ -66,7 +80,8 @@ class Slides extends Component {
     }
   }
 
-  handleClick = e => {
+  displayModal = e => {
+    this.props.toggleModal(true)
     return console.log(e.target)
   }
 
@@ -86,17 +101,57 @@ class Slides extends Component {
     }
   }
 
-  componentDidMount() {
-    //document.querySelector('.slide').addEventListener('click', this.handleClick(), false)
-    //let a = document.getElementsByClassName('slide')
-    //return console.log(a)
-  }
-
   render() {
     return(
       <div style={{...styles.slides, ...this.columnStyles()}}>
         {this.state.slides}
       </div>
+    )
+  }
+}
+
+class Modal extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      modalTop: 0,
+    }
+  }
+
+  handleScroll = e => {
+    let test = Math.abs(this.state.modalTop - e.pageY)
+
+    if ( test  > window.innerHeight * 0.66
+    && this.props.displayModal ) {
+      let modal = document.querySelector('#modal')
+      this.props.toggleModal(false)
+      console.log(modal.style.height)
+    }
+
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  componentDidUpdate() {
+    let modalTop = document.scrollingElement.scrollTop
+      + window.innerHeight * 0.025, modal = document.querySelector('#modal')
+    if (modal && this.state.modalTop !== modalTop) {
+      modal.style.top = `${modalTop}px`
+      this.setState({modalTop})
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  render(){
+    return (
+      this.props.displayModal ?
+        <div id='modal' style={styles.modal}/> :
+        null
     )
   }
 }
@@ -107,14 +162,17 @@ export default class App extends Component {
     this.state = {
       w: window.innerWidth,
       h: window.innerHeight,
+      displayModal: false,
     }
   }
-
-
 
   onResize = e => {
     let w = window.innerWidth, h = window.innerHeight
     this.setState({w, h})
+  }
+
+  toggleModal = displayModal => {
+    this.setState({displayModal})
   }
 
   componentDidMount() {
@@ -128,18 +186,19 @@ export default class App extends Component {
   render() {
 
     return (
-      <div style={styles.container}>
-        <InfoCard />
+      <div id='scroll' style={styles.container}>
+        <InfoCard displayModal={this.state.displayModal}
+          toggleModal={this.toggleModal}
+          />
         <div style={{...styles.container, ...styles.slideContainer}}>
-          <Slides />
+          <Modal displayModal={this.state.displayModal}
+            toggleModal={this.toggleModal} />
+          <Slides toggleModal={this.toggleModal} />
         </div>
       </div>
     )
   }
 }
-
-
-
 
 const styles = {
 
@@ -147,6 +206,8 @@ const styles = {
     width: '90vw',
   	height: 'auto',
     margin: 'auto',
+    backgroundColor: 'white',
+
   },
 
   slideContainer: {
@@ -168,5 +229,18 @@ const styles = {
     //backgroundColor: 'cyan',
     display: 'inline-grid',
     gridAutoFlow: 'row',
+    zIndex: 0,
+
+  },
+
+  modal: {
+    zIndex: 3,
+    backgroundColor: 'chartreuse',
+    width: '93vw',
+    height: '95vh',
+    position: 'absolute',
+    top: '2.5vh',
+    opacity: 0.7,
+
   },
 }
