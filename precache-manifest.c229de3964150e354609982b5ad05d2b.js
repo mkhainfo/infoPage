13186c7e7ef36d6764c33f8fca7ab5344953a3d8
mkhainfo <to@mkha.info>
2019-01-25 16:53:23 -0800

    Updates

---
+++ precache-manifest.c229de3964150e354609982b5ad05d2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "450144ece8a9a81c69e3",
+    "url": "/infoPage/static/js/runtime~main.450144ec.js"
+  },
+  {
+    "revision": "6a080318b6f58702be1f",
+    "url": "/infoPage/static/js/main.6a080318.chunk.js"
+  },
+  {
+    "revision": "6bd1ea98e814ca143deb",
+    "url": "/infoPage/static/js/1.6bd1ea98.chunk.js"
+  },
+  {
+    "revision": "6a080318b6f58702be1f",
+    "url": "/infoPage/static/css/main.4e0af70b.chunk.css"
+  },
+  {
+    "revision": "e9899e577e16619ce936271245b389e1",
+    "url": "/infoPage/index.html"
+  }
+];
